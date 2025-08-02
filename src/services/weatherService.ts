2895a77a30d595f723dcc3b3dd2c4bd500fa4df9
2GreekDevs <2greekdevs24@gmail.com>
2025-08-02 19:38:38 +0300

    Update weatherService.ts

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -1,4 +1,4 @@
-const API_KEY = "bd5e378503939ddaee76f12ad7a97608"; // OpenWeatherMap free API key
+const API_KEY = "YOUR_API_KEY"; // OpenWeatherMap free API key
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export interface WeatherResponse {
